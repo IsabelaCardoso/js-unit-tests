15d931f03a97f3e388e70b7693d08068af7e0cc9
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-09-30 17:41:55 -0300

    code climate

--- src/vqv.js
+++ src/vqv.js
@@ -17,11 +17,10 @@
 */
 
 const vqv = (nome, idade) => {
-  if ((nome == null && idade == null) || typeof(nome) !== 'string' || typeof(idade) !== 'number') {
-    return undefined
-  } else {
-    return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`
+  if ((nome == null && idade == null) || typeof (nome) !== 'string' || typeof (idade) !== 'number') {
+    return undefined;
   }
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
 
 module.exports = vqv;
--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -28,6 +28,7 @@ describe('#circle', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    
     // Teste se o objeto retornado tem 3 entradas.
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.

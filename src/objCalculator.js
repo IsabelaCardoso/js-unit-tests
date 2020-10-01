1922a23e4dd862e8487ec942e8c9f81168be293d
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-10-01 10:25:57 -0300

    refatora arrow function requisito 7 e adequa code climate

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,18 +20,10 @@
 */
 
 const calculator = {
-  add: (int1, int2) => {
-      return Math.floor(int1 + int2);
-  },
-  mult: (int1, int2) => {
-    return Math.floor(int1 * int2);
-  },
-  div: (int1, int2) => {
-    return Math.floor(int1 / int2);
-  },
-  sub: (int1, int2) => {
-    return Math.floor(int1 - int2);
-  }
+  add: (int1, int2) => Math.floor(int1 + int2),
+  mult: (int1, int2) => Math.floor(int1 * int2),
+  div: (int1, int2) => Math.floor(int1 / int2),
+  sub: (int1, int2) => Math.floor(int1 - int2)
 };
 
 module.exports = calculator;
